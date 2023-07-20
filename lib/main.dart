ca8cec80a919c6fe43f8795ab821d32e7b072dc6
patrick <patrick.zierahn@student.kit.edu>
2023-07-20 20:25:12 +0200

    Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:fuks_app/ui/pages/home/home_page.dart';
-// import 'package:fuks_app/ui/theme/theme.dart';
-//
-// void main() {
-//   runApp(const FuksApp());
-// }
-//
-// class FuksApp extends StatelessWidget {
-//   const FuksApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'fuks',
-//       theme: fuksThemeLight,
-//       darkTheme: fuksThemeDark,
-//       home: const HomePage(),
-//     );
-//   }
-// }
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
