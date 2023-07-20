bd08f9fd9ed70970ec1346eab0a71e846b5f7d63
patrick <patrick.zierahn@student.kit.edu>
2023-07-20 23:27:37 +0200

    Added isNoDeadlineExceeded function to error utils

--- lib/utils/error.dart
+++ lib/utils/error.dart
@@ -3,6 +3,14 @@ import 'dart:io';
 import 'package:grpc/grpc.dart';
 
 class ErrorUtils {
+  static bool isNoDeadlineExceeded(Object? error) {
+    if (error is GrpcError) {
+      return error.code == StatusCode.deadlineExceeded;
+    }
+
+    return false;
+  }
+
   static String toText(Object? error) {
     if (error is SocketException) {
       return error.message;
