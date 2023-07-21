a718cab0120fd9f0198c8db8931b706b8bf5b7f8
patrick <patrick.zierahn@student.kit.edu>
2023-07-21 15:52:29 +0200

    Update doorman address and cert

--- lib/services/doorman.dart
+++ lib/services/doorman.dart
@@ -1,30 +1,34 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:fuks_app/generated/google/protobuf/empty.pb.dart';
 import 'package:fuks_app/generated/doorman.pbgrpc.dart';
+import 'package:fuks_app/services/doorman_cert.dart';
+import 'package:grpc/grpc.dart';
 import 'package:grpc/grpc_or_grpcweb.dart';
 
-final _channel = GrpcOrGrpcWebClientChannel.toSingleEndpoint(
-  host: '192.168.0.125',
-  port: 50051,
-  transportSecure: false,
-);
-
-// final _channel = ClientChannel(
-//   '192.168.0.125',
+// final _channel = GrpcOrGrpcWebClientChannel.toSingleEndpoint(
+//   // host: '192.168.0.125',
+//   host: 'doorman.local',
 //   port: 50051,
-//   options: ChannelOptions(
-//     credentials: ChannelCredentials.secure(
-//       certificates: doormanCert.codeUnits,
-//       onBadCertificate: (certificate, host) {
-//         debugPrint('certificate: ${certificate.issuer}');
-//         debugPrint('host: $host');
-//         return host == '192.168.0.125:50051';
-//       },
-//     ),
-//   ),
+//   transportSecure: true,
 // );
+
+final _channel = ClientChannel(
+  'doorman.local',
+  port: 50051,
+  options: ChannelOptions(
+    credentials: ChannelCredentials.secure(
+      certificates: doormanCert.codeUnits,
+      onBadCertificate: (certificate, host) {
+        debugPrint('certificate: ${certificate.issuer}');
+        debugPrint('host: $host');
+        return host == 'doorman.local:50051';
+      },
+    ),
+  ),
+);
 
 final doorman = DoormanServiceWithToken();
 
