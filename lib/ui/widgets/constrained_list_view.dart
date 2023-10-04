2814abd00f1847d1e4f1687c126d9d7614413a01
patrick <patrick.zierahn@student.kit.edu>
2023-10-04 16:48:31 +0200

    Update ConstrainedListView

--- lib/ui/widgets/constrained_list_view.dart
+++ lib/ui/widgets/constrained_list_view.dart
@@ -5,14 +5,12 @@ class ConstrainedListView extends StatelessWidget {
   const ConstrainedListView({
     super.key,
     required this.children,
-    this.minWidth = 600,
     this.maxWidth = 800,
     this.shrinkWrap = false,
     this.primary,
   });
 
   final List<Widget> children;
-  final double minWidth;
   final double maxWidth;
   final bool shrinkWrap;
   final bool? primary;
@@ -20,38 +18,25 @@ class ConstrainedListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final breakpoint = Breakpoint.fromMediaQuery(context);
-    final media = MediaQuery.of(context);
 
-    double width = media.size.width;
-
-    if (breakpoint.window > WindowClass.medium || width >= maxWidth) {
-      return ListView(
-        shrinkWrap: shrinkWrap,
-        primary: primary,
-        children: children
-            .map((child) => Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: breakpoint.spacing,
-                      ),
-                      constraints: BoxConstraints(
-                        minWidth: minWidth,
-                        maxWidth: maxWidth,
-                      ),
-                      child: child,
-                    ),
-                  ],
-                ))
-            .toList(),
-      );
-    } else {
-      return ListView(
-        shrinkWrap: shrinkWrap,
-        primary: primary,
-        children: children,
-      );
-    }
+    return ListView(
+      shrinkWrap: shrinkWrap,
+      primary: primary,
+      children: children
+          .map(
+            (child) => Center(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: breakpoint.spacing,
+                ),
+                constraints: BoxConstraints(
+                  maxWidth: maxWidth,
+                ),
+                child: child,
+              ),
+            ),
+          )
+          .toList(),
+    );
   }
 }
