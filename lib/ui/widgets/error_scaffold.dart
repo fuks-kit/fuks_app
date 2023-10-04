8bc6d8ed7225faf389049882693fcded4f8d5a69
patrick <patrick.zierahn@student.kit.edu>
2023-10-04 16:48:55 +0200

    Added ErrorBody

--- lib/ui/widgets/error_scaffold.dart
+++ lib/ui/widgets/error_scaffold.dart
@@ -59,3 +59,48 @@ class ErrorScaffold extends StatelessWidget {
     );
   }
 }
+
+class ErrorBody extends StatelessWidget {
+  const ErrorBody({
+    super.key,
+    this.error,
+  });
+
+  final Object? error;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return SafeArea(
+      child: ListView(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        // mainAxisSize: MainAxisSize.max,
+        children: [
+          const SizedBox(
+            width: double.infinity,
+            height: 50,
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: 200,
+            width: 200,
+            child: UnDraw(
+              illustration: UnDrawIllustration.fixing_bugs,
+              color: colorScheme.error,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 26,
+            ),
+            alignment: Alignment.center,
+            child: Text(ErrorUtils.toText(error)),
+          ),
+        ],
+      ),
+    );
+  }
+}
