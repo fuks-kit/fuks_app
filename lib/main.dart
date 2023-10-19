4372d53586cf53c3e54e304b135a009aa1516985
patrick <patrick.zierahn@student.kit.edu>
2023-10-19 18:38:12 +0200

    Delete about page

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fuks_app/firebase_options.dart';
-import 'package:fuks_app/ui/pages/about/abouts_page.dart';
 import 'package:fuks_app/ui/pages/events/events_page.dart';
 import 'package:fuks_app/ui/pages/kt/kt_page.dart';
 import 'package:fuks_app/ui/pages/projects/projects_body.dart';
@@ -76,7 +75,6 @@ class _FuksAppState extends State<FuksApp> {
         ProjectPage.route: (context) => const ProjectPage(),
         ProjectsPage.route: (context) => const ProjectsPage(),
         KTPage.route: (context) => const KTPage(),
-        AboutPage.route: (context) => const AboutPage(),
       },
     );
   }
