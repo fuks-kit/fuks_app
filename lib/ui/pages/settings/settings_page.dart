e0876d9cbe7a9d679e7379b227f3a57d426d6612
patrick <patrick.zierahn@student.kit.edu>
2023-10-22 17:34:03 +0200

    Update version

--- lib/ui/pages/settings/settings_page.dart
+++ lib/ui/pages/settings/settings_page.dart
@@ -9,7 +9,7 @@ import 'package:fuks_app/utils/authenticate.dart';
 import 'package:fuks_app/utils/terms_and_privacy.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-const _version = '1.2.0';
+const _version = '1.2.1';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
