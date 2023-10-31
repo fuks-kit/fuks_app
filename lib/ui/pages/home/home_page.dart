7435ff8c0ccd3646701480819049eac4cae5d76d
patrick <patrick.zierahn@student.kit.edu>
2023-10-31 17:53:13 +0100

    Use bottom navigation bar on home screen

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -4,9 +4,7 @@ import 'package:fuks_app/ui/pages/kt/kt_page.dart';
 import 'package:fuks_app/ui/pages/projects/projects_body.dart';
 import 'package:fuks_app/ui/pages/office/office_page.dart';
 import 'package:fuks_app/ui/pages/settings/settings_page.dart';
-import 'package:fuks_app/ui/widgets/constrained_list_view.dart';
 import 'package:fuks_app/ui/widgets/fuks_logo.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,27 +16,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    final textTheme = Theme.of(context).textTheme;
-
-    final titleStyle = textTheme.titleMedium?.copyWith(
-      fontWeight: FontWeight.w700,
-    );
-    final subtitleStyle = textTheme.bodySmall?.merge(TextStyle(
-      color: colorScheme.outline,
-    ));
 
     return Scaffold(
       appBar: AppBar(
         title: const FuksLogo(),
+        centerTitle: false,
         actions: [
-          IconButton(
-            icon: const Icon(Icons.key),
-            tooltip: 'Büro Zugang',
-            onPressed: () => OfficePage.show(context),
-          ),
           IconButton(
             icon: const Icon(Icons.settings),
             tooltip: 'Einstellungen',
@@ -46,93 +34,50 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: ConstrainedListView(
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Erfolgreiche studentische Unternehmensberatung seit über 25 Jahren',
-              style: textTheme.titleMedium,
-            ),
+          const Divider(height: 1),
+          NavigationBar(
+            elevation: 2,
+            selectedIndex: _selectedIndex,
+            height: 56,
+            indicatorColor: colorScheme.outlineVariant,
+            labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+            onDestinationSelected: (index) =>
+                setState(() => _selectedIndex = index),
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.calendar_month_outlined),
+                selectedIcon: Icon(Icons.calendar_month),
+                label: 'Events',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.lightbulb_outline),
+                selectedIcon: Icon(Icons.lightbulb),
+                label: 'Projekte',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.newspaper_outlined),
+                selectedIcon: Icon(Icons.newspaper),
+                label: 'Karlsruher Transfer',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.key_outlined),
+                selectedIcon: Icon(Icons.key),
+                label: 'Büro',
+              ),
+            ],
           ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 8,
-              vertical: 32,
-            ),
-            alignment: Alignment.center,
-            child: const FuksIcon(
-              size: 100,
-            ),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.info_outline,
-              color: colorScheme.primary,
-            ),
-            minLeadingWidth: 32,
-            title: Text(
-              'Mehr über fuks',
-              style: titleStyle,
-            ),
-            subtitle: Text(
-              'https://fuks.org',
-              style: subtitleStyle,
-            ),
-            onTap: () => launchUrlString('https://fuks.org'),
-          ),
-          const SizedBox(height: 8),
-          ListTile(
-            leading: Icon(
-              Icons.event,
-              color: colorScheme.primary,
-            ),
-            minLeadingWidth: 32,
-            title: Text(
-              'Events',
-              style: titleStyle,
-            ),
-            subtitle: Text(
-              'Nimm an einem unseren Events teil',
-              style: subtitleStyle,
-            ),
-            onTap: () => EventsPage.show(context),
-          ),
-          const SizedBox(height: 8),
-          ListTile(
-            leading: Icon(
-              Icons.lightbulb_outline,
-              color: colorScheme.primary,
-            ),
-            minLeadingWidth: 32,
-            title: Text(
-              'Projekte',
-              style: titleStyle,
-            ),
-            subtitle: Text(
-              'Bringe dein Wissen in unsere Projekte ein',
-              style: subtitleStyle,
-            ),
-            onTap: () => ProjectsPage.show(context),
-          ),
-          const SizedBox(height: 8),
-          ListTile(
-            leading: Icon(
-              Icons.newspaper,
-              color: colorScheme.primary,
-            ),
-            minLeadingWidth: 32,
-            title: Text(
-              'Karlsruhe Transfer',
-              style: titleStyle,
-            ),
-            subtitle: Text(
-              'Lese das Magazin für Studierende',
-              style: subtitleStyle,
-            ),
-            onTap: () => KTPage.show(context),
-          ),
+        ],
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: const [
+          EventsBody(),
+          ProjectsBody(),
+          KTBody(),
+          OfficeBody(),
         ],
       ),
     );
