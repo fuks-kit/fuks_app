68648b3a9a66631a69e1678f300f7778245f83ab
patrick <patrick.zierahn@student.kit.edu>
2023-11-14 18:10:40 +0100

    New access view

--- lib/ui/pages/office/access.dart
+++ lib/ui/pages/office/access.dart
@@ -4,6 +4,7 @@ import 'package:fuks_app/services/doorman.dart';
 import 'package:fuks_app/ui/widgets/error.dart';
 import 'package:fuks_app/ui/pages/office/access_timer.dart';
 import 'package:fuks_app/ui/widgets/constrained_list_view.dart';
+import 'package:fuks_app/ui/widgets/outlined_card.dart';
 import 'package:undraw/undraw.dart';
 
 class AccessBody extends StatefulWidget {
@@ -35,97 +36,124 @@ class _AccessBodyState extends State<AccessBody> {
     ));
   }
 
+  void _onDoorOpen() {
+    setState(() {
+      _loading = true;
+    });
+
+    final req = DoorOpenRequest();
+    req.challenge = widget.challenge;
+
+    doorman.openDoor(req).then(_onSuccess).catchError(
+      (error, trace) {
+        setState(() => _loading = false);
+        showErrorInfo(context, error, trace);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    final textStyle = Theme.of(context).textTheme;
+    return Theme(
+      data: ThemeData(
+        colorSchemeSeed: Colors.greenAccent,
+        brightness: Theme.of(context).brightness,
+      ),
+      child: Builder(
+        builder: (context) {
+          final colorScheme = Theme.of(context).colorScheme;
 
-    return ConstrainedListView(
-      shrinkWrap: true,
-      primary: false,
-      maxWidth: 400,
-      children: [
-        const UnDraw(
-          width: 288,
-          height: 192,
-          padding: EdgeInsets.all(8),
-          illustration: UnDrawIllustration.fingerprint_login,
-          color: Colors.greenAccent,
-        ),
-        const SizedBox(height: 24),
-        ListTile(
-          title: Text(
-            'Du hast Zugang zum Büro!',
-            textAlign: TextAlign.start,
-            style: textStyle.headlineSmall?.merge(const TextStyle(
-              fontWeight: FontWeight.w600,
-            )),
-          ),
-          subtitle: Text(
-            'Achte auf folgende Punkte:',
-            textAlign: TextAlign.start,
-            style: textStyle.bodySmall?.copyWith(
-              color: colorScheme.outline,
-            ),
-          ),
-        ),
-        const ListTile(
-          leading: Icon(
-            Icons.meeting_room,
-            color: Colors.greenAccent,
-            // color: colorScheme.outline,
-          ),
-          title: Text('Schließe alle Türen und Fenster'),
-        ),
-        const ListTile(
-          leading: Icon(
-            Icons.delete,
-            color: Colors.greenAccent,
-          ),
-          title: Text('Entsorge deinen Müll'),
-        ),
-        const ListTile(
-          leading: Icon(
-            Icons.light,
-            color: Colors.greenAccent,
-          ),
-          title: Text('Schalte das Licht aus'),
-        ),
-        const SizedBox(height: 16),
-        ListTile(
-          leading: _loading
-              ? const SizedBox(
-                  width: 24,
-                  height: 24,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                  ),
-                )
-              : const Icon(Icons.key, color: Colors.black),
-          title: const Text(
-            'Tür öffnen',
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
-          tileColor: Colors.greenAccent,
-          onTap: () {
-            setState(() {
-              _loading = true;
-            });
-
-            final req = DoorOpenRequest();
-            req.challenge = widget.challenge;
-
-            doorman.openDoor(req).then(_onSuccess).catchError(
-              (error, trace) {
-                setState(() => _loading = false);
-                showErrorInfo(context, error, trace);
-              },
-            );
-          },
-        ),
-      ],
+          return ConstrainedListView(
+            shrinkWrap: true,
+            primary: false,
+            maxWidth: 400,
+            children: [
+              UnDraw(
+                width: 192,
+                height: 192,
+                padding: const EdgeInsets.all(8),
+                illustration: UnDrawIllustration.fingerprint,
+                color: colorScheme.primary,
+              ),
+              const SizedBox(height: 24),
+              OutlinedCard(
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(
+                        'Zugang gewährt',
+                        style: TextStyle(
+                          color: colorScheme.onPrimaryContainer,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      subtitle: Text(
+                        'Achte auf folgende Punkte:',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: colorScheme.onPrimaryContainer,
+                        ),
+                      ),
+                      // tileColor: colorTheme.primaryContainer,
+                      tileColor: colorScheme.primaryContainer,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(8),
+                          topRight: Radius.circular(8),
+                        ),
+                      ),
+                    ),
+                    const Divider(
+                      height: 1,
+                      // height: 0.5,
+                      // thickness: 0.5,
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.meeting_room,
+                        color: colorScheme.primary,
+                        // color: colorScheme.outline,
+                      ),
+                      title: const Text('Schließe alle Türen und Fenster'),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.delete,
+                        color: colorScheme.primary,
+                      ),
+                      title: const Text('Entsorge deinen Müll'),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.light,
+                        color: colorScheme.primary,
+                      ),
+                      title: const Text('Schalte das Licht aus'),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 24),
+              SizedBox(
+                width: double.infinity,
+                child: FilledButton.icon(
+                  onPressed: _onDoorOpen,
+                  icon: _loading
+                      ? SizedBox(
+                          width: 24,
+                          height: 24,
+                          child: CircularProgressIndicator(
+                            color: colorScheme.primaryContainer,
+                          ),
+                        )
+                      : const Icon(Icons.key),
+                  label: const Text('Tür Öffnen'),
+                ),
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
