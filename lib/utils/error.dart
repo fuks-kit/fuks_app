f84150192ae7a7fbbf9e99557f282c4b062d5628
patrick <patrick.zierahn@student.kit.edu>
2023-12-01 18:42:43 +0100

    Remove dead code

--- lib/utils/error.dart
+++ lib/utils/error.dart
@@ -3,26 +3,6 @@ import 'dart:io';
 import 'package:grpc/grpc.dart';
 
 class ErrorUtils {
-  static bool isDeadlineExceeded(Object? error) {
-    if (error is GrpcError) {
-      return error.code == StatusCode.deadlineExceeded;
-    }
-
-    return false;
-  }
-
-  static bool isUnavailable(Object? error) {
-    if (error is GrpcError) {
-      return error.code == StatusCode.unavailable;
-    }
-
-    return false;
-  }
-
-  static bool isNotConnected(Object? error) {
-    return isUnavailable(error) || isDeadlineExceeded(error);
-  }
-
   static String toText(Object? error) {
     if (error is SocketException) {
       return error.message;
